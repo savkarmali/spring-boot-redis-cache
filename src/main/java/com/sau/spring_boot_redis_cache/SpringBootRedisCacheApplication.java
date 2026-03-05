019eb64b4031e7855bd84bd43a85013885139cad
savkarmali <savkarmali3@gmail.com>
2026-03-06 01:43:20 +0530

    Added docker-compose configuration for Redis

--- src/main/java/com/sau/spring_boot_redis_cache/SpringBootRedisCacheApplication.java
+++ src/main/java/com/sau/spring_boot_redis_cache/SpringBootRedisCacheApplication.java
@@ -2,8 +2,10 @@ package com.sau.spring_boot_redis_cache;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class SpringBootRedisCacheApplication {
 
 	public static void main(String[] args) {
